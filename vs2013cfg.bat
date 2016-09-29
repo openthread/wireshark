d0121f5dd0ab0a3296f24077b6da169a4de9fa0d
Robert Cragie <Robert.Cragie@arm.com>
2016-09-29 12:03:35 +0100

    Minor fixes to batch files

--- vs2013cfg.bat
+++ vs2013cfg.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set CYGWIN=nodosfilewarning
-set PATH="C:\Program Files\Git\bin";%PATH%
-set WIRESHARK_CYGWIN_INSTALL_PATH=C:\cygwin64\bin
+set PATH="C:\Program Files\Git\cmd";%PATH%
+set WIRESHARK_CYGWIN_INSTALL_PATH=C:\cygwin64
 set WIRESHARK_BASE_DIR=C:\Users\robcra01\Documents\WiresharkBitbucket
 set WIRESHARK_TARGET_PLATFORM=win32
 set WIRESHARK_VERSION_EXTRA=-Thread
--- vs2013cmcfg.bat
+++ vs2013cmcfg.bat
@@ -1,9 +1,10 @@
 @echo off
 
 set CYGWIN=nodosfilewarning
+set PATH="C:\Program Files\Git\cmd";%PATH%
 set PATH="C:\Program Files (x86)\CMake\bin";%PATH%
 set QT5_BASE_DIR=C:\Qt\5.6\msvc2013_64
-set WIRESHARK_CYGWIN_INSTALL_PATH=C:\cygwin64\bin
+set WIRESHARK_CYGWIN_INSTALL_PATH=C:\cygwin64
 set WIRESHARK_BASE_DIR=C:\Users\robcra01\Documents\WiresharkBitbucket
 set WIRESHARK_TARGET_PLATFORM=win64
 set PLATFORM=x64
